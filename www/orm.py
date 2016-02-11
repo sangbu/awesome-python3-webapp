b32ea0d870fcf2e10980c220b14b493ab4ef76e2
sangbu <969681135@qq.com>
2016-02-11 14:59:28 +0800

    数据库信息改为配置文件读取

--- www/orm.py
+++ www/orm.py
@@ -15,8 +15,8 @@ def create_pool(loop,**kw):
 	__pool = yield from aiomysql.create_pool(
 		host = kw.get('host','localhost'),
 		port = kw.get('port','3306'),
-		user = kw['root'],
-		password = kw['1234'],
+		user = kw['user'],
+		password = kw['password'],
 		db = kw['db'],
 		charset = kw.get('charset','utf8'),
 		autocommit = kw.get('autocommit', True),
